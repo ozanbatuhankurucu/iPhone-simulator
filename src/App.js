6bee92e3f8e2bd258a03d37cd546e67bf19ab11a
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-04-22 15:08:04 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import bgImg from './img/bg-00.jpeg'
+import navbarImg from './img/Navbar.png'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='device device-iphone-x phone-iphone-x'>
+        <div className='device-frame'>
+          <div className='device-content'>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Libero nam velit iure eaque error vitae officia nobis unde fugiat in et corrupti, quam
+              porro asperiores aperiam minus voluptate amet nulla. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Libero nam velit iure eaque error vitae officia nobis
+              unde fugiat in et corrupti, quam porro asperiores aperiam minus voluptate amet nulla.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Libero nam velit iure eaque error vitae officia nobis unde fugiat in et corrupti, quam
+              porro asperiores aperiam minus voluptate amet nulla. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Libero nam velit iure eaque error vitae officia nobis
+              unde fugiat in et corrupti, quam porro asperiores aperiam minus voluptate amet nulla.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Libero nam velit iure eaque error vitae officia nobis unde fugiat in et corrupti, quam
+              porro asperiores aperiam minus voluptate amet nulla. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Libero nam velit iure eaque error vitae officia nobis
+              unde fugiat in et corrupti, quam porro asperiores aperiam minus voluptate amet nulla.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam velit iure eaque
+              error vitae officia nobis unde fugiat in et corrupti, quam porro asperiores aperiam
+              minus voluptate amet nulla.
+            </div>
+          </div>
+        </div>
+        <div className='device-stripe'></div>
+        <div className='device-header'></div>
+        <div className='device-sensors'></div>
+        <div className='device-btns'></div>
+        <div className='device-power'></div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/FakeDevice.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FakeDevice = () => {
+  return <div>fakedevice</div>
+}
+
+export default FakeDevice
